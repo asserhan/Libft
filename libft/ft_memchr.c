c98d19ed766f21d3905439eae46891ff8f2cfb9d
Hanane Asseraou <hasserao@e1r5p11.1337.ma>
2022-10-10 23:45:14 +0100

    new

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -9,5 +9,33 @@
 /*   Updated: 2022/10/10 21:42:01 by hasserao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	//(char *)p;
+	size_t i;
+
+	i = 0;
+	while (i <= n)
+	{
+		if (((char *)s)[i] == c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	return (NULL);
+}
+int main ()
+{
+	char p[] = "life is hard";
+	char *ptr;
+	char *ptr1;
+	ptr = ft_memchr(p,'\0',ft_strlen (p));
+	printf("%s\n",ptr);
+	ptr1 = memchr(p,'\0',ft_strlen (p));
+	printf("%s",ptr1);
+
+}
 
